3ea6cc0dc8482842711ff1adb5912b9fead6a38f
Tommy Chu <tommychu2256@gmail.com>
2025-12-14 23:25:59 +0100

    Add meditron datasets

--- 01-dataset-download/reproduce_apertus_downloads.sh
+++ 01-dataset-download/reproduce_apertus_downloads.sh
@@ -10,24 +10,36 @@ export MAX_RETRIES=15
 DATASET_NAME="mvp-lab/LLaVA-OneVision-1.5-Mid-Training-85M" sbatch 01-dataset-download/download_hf_dataset.slurm
 
 # 2) Download Medical datasets
-#
-# HF well-formed datasets
+# a. HF well-formed datasets
 DATASET_NAME="OpenMeditron/Mediset" sbatch 01-dataset-download/download_hf_dataset.slurm
 DATASET_NAME="randall-lab/medmnist" TRUST_REMOTE_CODE="true" sbatch 01-dataset-download/download_hf_dataset.slurm
 DATASET_NAME="allenai/pixmo-ask-model-anything" sbatch 01-dataset-download/download_hf_dataset.slurm
 #
-# HF ill-formed datasets
+# b. HF ill-formed datasets
 mkdir -p $MEDICAL_DIR/raw
 hf download cyd0806/BIMCV-R --repo-type dataset --local-dir $MEDICAL_DIR/raw/BIMCV-R
 hf download xmcmic/PMC-VQA --repo-type dataset --local-dir $MEDICAL_DIR/raw/PMC-VQA
 #
-# Non-HF datasets
+# c. Non-HF datasets
 zenodo_get https://doi.org/10.5281/zenodo.15814064 -o $MEDICAL_DIR/raw/MultiCaRe
 #
-# Datasets with manual download (due to ToS, licensing, etc)
-echo "Download the following datasets manually:"
-echo "1) MURA (MSK Xrays): https://stanfordaimi.azurewebsites.net/datasets/3e00d84b-d86e-4fed-b2a4-bfe3effd661b"
-echo "2) MRNet (Knee MRI's): https://stanfordaimi.azurewebsites.net/datasets/bface6fc-7859-47d7-a1c8-022cd6b17419"
-echo "3) LERA (Lower Extremity Radiographs): https://stanfordaimi.azurewebsites.net/datasets/44a63ddf-edd8-46cb-b021-094bb8efb802"
-echo "4) CoMM Dataset: Follow instructions at https://github.com/HKUST-LongGroup/CoMM"
-
+# d. Datasets with manual download (due to ToS, licensing, etc)
+#   - MURA (MSK Xrays):                    https://stanfordaimi.azurewebsites.net/datasets/3e00d84b-d86e-4fed-b2a4-bfe3effd661b
+#   - MRNet (Knee MRI's):                  https://stanfordaimi.azurewebsites.net/datasets/bface6fc-7859-47d7-a1c8-022cd6b17419
+#   - LERA (Lower Extremity Radiographs):  https://stanfordaimi.azurewebsites.net/datasets/44a63ddf-edd8-46cb-b021-094bb8efb802
+#   - CoMM Dataset:                        https://github.com/HKUST-LongGroup/CoMM
+#
+# e. Datasets shared by the Meditron team (to be published on HF later)
+#   - BUSI
+#   - COVID_US
+#   - DDTI
+#   - PMC_VQA
+#   - ct2
+#   - image_mammoth
+#   - iu_xray
+#   - llava_instruct
+#   - llava_pretrain_cleaned
+#   - medtrinity_conversations_1_formatted
+#   - medtrinity_conversations_2_formatted
+#   - pixmo_anything
+#   - pixmo_cap
